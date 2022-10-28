5bee2c4bea61523a96f0e571205fe1b63b6387f4
JDomene99 <jdomene99@gmail.com>
2022-10-28 12:17:26 +0200

    bui

--- Desarollo_Entorno_Cliente/Practica4 copy 2/js/pruebaClase.js
+++ Desarollo_Entorno_Cliente/Practica4 copy 2/js/pruebaClase.js
@@ -3,89 +3,150 @@
  * Clave puede ser de cualquier tipo
  * 
  */
-const usuario = {
-  nombre : 'carlos',
-  ocupacion : 'profesor',
-};
-//crear un map simple
-// const carlos = new Map(['nombre','carlos'], ['ocupacion','profesor']);
+// const usuario = {
+//   nombre : 'carlos',
+//   ocupacion : 'profesor',
+// };
+// //crear un map simple
+// // const carlos = new Map(['nombre','carlos'], ['ocupacion','profesor']);
 
-//como crear un map utilizando u obj
-const misUsuarios = new Map(Object.entries(usuario));
+// //como crear un map utilizando u obj
+// const misUsuarios = new Map(Object.entries(usuario));
 
-//convertir un map a obj
-// const myArray = new Map(Object.from(usuario));
-
-
-const mapVacio = new Map();
-console.log(mapVacio.size);
-
-//averigguar si una clave esta en el map
-mapVacio.has('lo que quiera buscar');
-
-//sacar el valor de una clave,borrar,borrar todo,añadir elemento al map
-mapVacio.get('')
-mapVacio.delete('');
-mapVacio.clear();
-mapVacio.set('clave', 'valor');
+// //convertir un map a obj
+// // const myArray = new Map(Object.from(usuario));
 
 
-// Utilizando Map crear una estrutura de datos que recoja los clientes de un centro deportivo para su ficha de registro.Debemos conoceer
-// nombre,apellidos,telefono,edad,sexo,peso,altura
-//1.- crear una funcion que permita crear un usuario untilizando map
-const datosUsuario = new Map();
+// const mapVacio = new Map();
+// console.log(mapVacio.size);
 
-function addUser(nombre,apellidos,edad,telefono,sexo,peso,altura) {
+// //averigguar si una clave esta en el map
+// mapVacio.has('lo que quiera buscar');
+
+// //sacar el valor de una clave,borrar,borrar todo,añadir elemento al map
+// mapVacio.get('')
+// mapVacio.delete('');
+// mapVacio.clear();
+// mapVacio.set('clave', 'valor');
+
+
+// // Utilizando Map crear una estrutura de datos que recoja los clientes de un centro deportivo para su ficha de registro.Debemos conoceer
+// // nombre,apellidos,telefono,edad,sexo,peso,altura
+// //1.- crear una funcion que permita crear un usuario untilizando map
+// const datosUsuario = new Map();
+
+// function addUser(nombre,apellidos,edad,telefono,sexo,peso,altura) {
   
-  datosUsuario.set('nombre', nombre);
-  datosUsuario.set('apellidos', apellidos);
-  datosUsuario.set('edad', edad);
-  datosUsuario.set('telefono', telefono);
-  datosUsuario.set('sexo', sexo);
-  datosUsuario.set('peso', peso);
-  datosUsuario.set('altura', altura);
-}
+//   datosUsuario.set('nombre', nombre);
+//   datosUsuario.set('apellidos', apellidos);
+//   datosUsuario.set('edad', edad);
+//   datosUsuario.set('telefono', telefono);
+//   datosUsuario.set('sexo', sexo);
+//   datosUsuario.set('peso', peso);
+//   datosUsuario.set('altura', altura);
+// }
 
-addUser('pedro','jimenez','13','16511','m','90','190');
-console.log(datosUsuario);
-
-
-// const datosUsuario2 = new Map();
-// datosUsuario2.set('');
+// addUser('pedro','jimenez','13','16511','m','90','190');
+// console.log(datosUsuario);
 
 
-//alamcene el inidce pluviometrico de los meses del año
-const meses = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre'];
-const indice = new Set();
-for(item of meses){
-  indice.add([item,  Math.floor(Math.random() * (101 - 1) + 1)]);
-}
+// // const datosUsuario2 = new Map();
+// // datosUsuario2.set('');
 
 
-const alumno  = new Map( [ ['nombre', 'carlos'],['edad',45],['sexo','v'] ] );
+// //alamcene el inidce pluviometrico de los meses del año
+// const meses = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre'];
+// const indice = new Set();
+// for(item of meses){
+//   indice.add([item,  Math.floor(Math.random() * (101 - 1) + 1)]);
+// }
 
-alumno.forEach( (element, key) => {   
-  console.log(element);
+
+// const alumno  = new Map( [ ['nombre', 'carlos'],['edad',45],['sexo','v'] ] );
+
+// alumno.forEach( (element, key) => {   
+//   console.log(element);
     
-});
+// });
 
-for(item of alumno.values()){
-  console.log(item);
+// for(item of alumno.values()){
+//   console.log(item);
    
-}
+// }
 
 
-const indicePlu = new Map( [['enero',100], ['febrero',100], ['marzo',80]] );
-const setMeses = new Set();
-let suma = 0;
-for(let item of indicePlu.values()){
-  setMeses.add(item)
-  if(setMeses.add(item)){
-    suma +=item;  
-    console.log(suma);
-  }
+// const indicePlu = new Map( [['enero',100], ['febrero',100], ['marzo',80]] );
+// const setMeses = new Set();
+// let suma = 0;
+// for(let item of indicePlu.values()){
+//   setMeses.add(item)
+//   if(setMeses.add(item)){
+//     suma +=item;  
+//     console.log(suma);
+//   }
+  
+// }
+// console.log(setMeses);
+// console.log(suma/setMeses.size);
+
+// map a obje
+// const obj = Object.fromEntries(map);
+
+// //map a array
+// const array = Array.from(map);
+
+
+const array2 = [1,1,1,2,3,4];
+const newArray2 = [... new Set(array2)];
+console.log(newArray2);
+
+
+const alumnos = [ { nombre : 'pedro', edad: 34,}, 
+{ nombre: 'javi' , edad: 34},
+{nombre : 'luis', edad: 30}, 
+];
+
+const alumnosNoRep = new Set();
+for(let alumno of alumnos){
+  alumnosNoRep.add(alumno.edad);
   
 }
-console.log(setMeses);
-console.log(suma/setMeses.size);
+const miArray = [... alumnosNoRep];
+console.log(miArray);
+
+//otra forma
+
+const set =  new Set();
+alumnos.forEach( 
+  alumno => set.add(alumno.edad)
+);
+console.log(set);
+
+const set2 = [...new Set(alumnos.map(alumno => alumno.edad ))];
+
+const aerolineas = [
+  { nombre: 'Iberia',
+    AeropuertoBase: 'Barajas',
+    Pais: 'ES',
+    SalidasA : {Granada: 17, Barcelona: 19, Paris: 21 },
+    LlegadasD : {Paris: 23, Barcelona: 17, Londres: 14 },
+  }, 
+
+];
+ 
+const pureba = aerolineas.from()
+
+
+const mapAerolineas = new Map([aerolineas]);
+
+// const preuba = aerolineas.forEach((value, key));
+// console.log(preuba);
+for(item of aerolineas){
+    console.log(item);
+}
+// console.log(mapAerolineas);
+
+// for(item of mapAerolineas){
+//   console.log(item);
+// }
 
