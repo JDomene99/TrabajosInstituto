aaa3b5f09e2f6505a0c6dd386d36fcfc4a6696d1
JDomene99 <jdomene99@gmail.com>
2022-10-28 19:01:59 +0200

    ada

--- Desarollo_Entorno_Servidor/Practicas6/models/valoracionRepository.php
+++ Desarollo_Entorno_Servidor/Practicas6/models/valoracionRepository.php
@@ -14,18 +14,18 @@ class ValoracionRepository{
     }
 
     public static function setValoracion($idUser,$idArticle,$valoracion ){
-        $entra = ValoracionRepository::comprobarSiHaValorado($idUser);
-                
+        $entra = ValoracionRepository::comprobarSiHaValorado($idUser, $idArticle);
         $db = Conectar::conexion();  
         //si no ha introducido ya una valoracion la introduce        
         if($entra){
+            echo 'asdas';
             $result = $db->query("INSERT INTO valoracion (id_user, id_article, valoracion) VALUES ('$idUser', '$idArticle', '$valoracion')");
         }          
     }
 
-    public static function comprobarSiHaValorado($idUser){
+    public static function comprobarSiHaValorado($idUser, $idArticle){
         $db = Conectar::conexion();
-        $q = "SELECT  valoracion FROM valoracion where id_user = '".$idUser."' ";
+        $q = "SELECT  valoracion FROM valoracion where id_user = '".$idUser."'AND id_article = '".$idArticle."' ";
         $result = $db->query($q);
         $datos = $result->fetch_row();
         $valoracion;
