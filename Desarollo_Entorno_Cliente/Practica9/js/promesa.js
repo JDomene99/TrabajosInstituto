fcb4d2c499d94f5027be40b95fc27006e2adbee6
JDomene99 <jdomene99@gmail.com>
2022-12-09 10:47:29 +0100

    dasd

--- Desarollo_Entorno_Cliente/Practica9/js/promesa.js
+++ Desarollo_Entorno_Cliente/Practica9/js/promesa.js
@@ -5,8 +5,6 @@ const $body = document.querySelector('tbody')
 const $template = document.querySelector('.template').content;
 
 
-
-
 const getAll = async () => {
 
     const endPoint = 'http://localhost:4000/product';
@@ -22,9 +20,6 @@ const getAll = async () => {
         jsonResponse.forEach(element => {
             $template.querySelector('.nameTable').textContent = element.name;
             $template.querySelector('.precioTable').textContent = element.precio;
-            //clonamos e importamos el nombre
-            let clonado = document.importNode($template, true);
-            $fragement.appendChild(clonado);
 
             //añadimos data-attributte con los elementos id, name, precio
             //para updatear
@@ -35,9 +30,22 @@ const getAll = async () => {
             // para borrar sol necesito el id
             $template.querySelector('.delete').dataset.id = element.id;
 
-        });
+            //clonamos e importamos el nombre
+            let clonado = document.importNode($template, true);
+            $fragement.appendChild(clonado);
 
+        });
         $body.appendChild($fragement);
+
+        //funcion para borrar
+        const $deleteButtons = document.querySelectorAll('.delete');
+        deleteItem($deleteButtons);
+
+        //funcion para editar
+        const $editButtons = document.querySelectorAll('.editar');
+        editItem($editButtons);
+
+
 
 
     } catch (e) {
@@ -48,17 +56,13 @@ const getAll = async () => {
 //cuando carga la pagina
 document.addEventListener("DOMContentLoaded", getAll);
 
-
-//delete 
-// const $delete = document.querySelector('.delete');
+//insertar datos 
 document.addEventListener('submit', async (e) => {
 
-    //insertar datos 
-    console.log(e.target);
-    console.log(e);
     if (e.target == $form) {
         e.preventDefault();
-
+        console.log(e.target.id.value);
+        console.log(e.target.name.value);
         try {
             //cabecera
             const options = {
@@ -66,8 +70,14 @@ document.addEventListener('submit', async (e) => {
                 headers: { "Content-Type": "application/json; charset=utf-8" },
                 body: JSON.stringify({ name: e.target.name.value, precio: e.target.precio.value, })
             };
-            console.log(e.target.name.value);
-            const endPoint = 'http://localhost:4000/product';
+            let endPoint = '';
+            // if (e.target.id.value != undefined) {
+            //     endPoint = 'http://localhost:4000/product' + e.target.id.value;
+            // }
+            // else {
+            endPoint = 'http://localhost:4000/product';
+            // }
+
             const response = await fetch(endPoint, options);
             const jsonResponse = await response.json();
 
@@ -78,35 +88,77 @@ document.addEventListener('submit', async (e) => {
             location.reload();
 
         } catch (error) {
-
+            console.log(error.message);
         }
     }
-
-    if (e.target == $template) {
-        console.log('ada');
-        e.preventDefault();
-        // try {
-        //     //cabecera
-        //     const options = {
-        //         method: "POST",
-        //         headers: { "Content-Type": "application/json; charset=utf-8" },
-        //         body: JSON.stringify({ name: e.target.name.value, precio: e.target.precio.value, })
-        //     };
-        //     console.log(e.target.name.value);
-        //     const endPoint = 'http://localhost:4000/product';
-        //     const response = await fetch(endPoint, options);
-        //     const jsonResponse = await response.json();
-
-        //     if (!response.ok) {
-        //         throw { status: response.status, statusText: response.statusText }
-        //     }
-        //     //recargamos la pagina una vez que la peticion post ha sido satisfecha
-        //     location.reload();
-
-        // } catch (error) {
-
-        // }
-    }
-    //borar datos
-
 });
+
+function deleteItem(deleteButtons) {
+    deleteButtons.forEach(button => {
+
+        button.addEventListener('click', async (e) => {
+            const options = {
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" },
+            };
+
+            const url =
+                "http://localhost:4000/product/" + button.dataset.id;
+
+            try {
+                const res = await fetch(url, options);
+                const json = await res.json();
+
+                if (!res.ok) {
+                    throw {
+                        status: res.status,
+                        statusText: res.statusText,
+                    };
+                }
+            } catch (error) {
+                console.log(error.message);
+            }
+        });
+
+    });
+}
+
+const $nameItem = document.querySelector('#nameItem');
+const $precioItem = document.querySelector('#precioItem');
+const $idItem = document.querySelector('#idItem');
+function editItem(editButton) {
+    editButton.forEach(button => {
+
+        button.addEventListener('click', async (e) => {
+            // const options = {
+            //     method: "UPDATE",
+            //     headers: { "Content-Type": "application/json" },
+            // };
+
+            // const url =
+            //     "http://localhost:4000/product/" + button.dataset.id;
+
+            // try {
+            //     const res = await fetch(url, options);
+            //     const json = await res.json();
+
+            //     if (!res.ok) {
+            //         throw {
+            //             status: res.status,
+            //             statusText: res.statusText,
+            //         };
+            //     }
+            // } catch (error) {
+            //     console.log(error.message);
+            // }
+            $nameItem.value = button.dataset.name;
+            $precioItem.value = button.dataset.precio;
+            $idItem.value = button.dataset.id;
+
+            console.log(button.dataset.id);
+            console.log(button.dataset.name);
+            console.log(button.dataset.precio);
+        });
+
+    });
+}
